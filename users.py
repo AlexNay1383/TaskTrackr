89bdbc708699728b9a12f07b0e612a2801a3e44a
AlexNay1383 <alexnay1383@gmail.com>
2024-06-09 13:35:27 +0300

    Added .gitignore

--- users.py
+++ users.py
@@ -1,9 +1,9 @@
 
 
 class User:
-    def __init__(self, username, email, phash) -> None:
-        self.email = email
-        self.phash = phash
-        self.username = username
+    def __init__(self) -> None:
+        self.email = None
+        self.phash = None
+        self.username = None
 
 users = {}
